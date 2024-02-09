7718b1dc63dd5de352add34b07a904e2ad0caf5e
Sarah French <sarah.french@hashicorp.com>
2024-02-09 11:57:38 +0000

    Remove intentionally failing test

--- mmv1/third_party/terraform/.teamcity/tests/nightly_tests_project.kt
+++ mmv1/third_party/terraform/.teamcity/tests/nightly_tests_project.kt
@@ -10,13 +10,6 @@ import projects.googleCloudRootProject
 
 class NightlyTestProjectsTests {
 
-
-    // Add failing test to be picked up by GHA
-    @Test
-    fun thisTestFails() {
-        assertTrue("This test will fail", false)
-    }
-
     @Test
     fun allBuildsShouldHaveTrigger() {
         val project = googleCloudRootProject(testContextParameters())
